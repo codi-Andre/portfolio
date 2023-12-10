37b763ce13e0fd843f2bd5b029ad0927a4cb60c5
André Miranda <aegis.andre@gmail.com>
2023-12-10 19:58:33 -0300

    build: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{svelte,js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "0.5rem"
+    },
+    extend: {}
+  },
+  plugins: []
+}
