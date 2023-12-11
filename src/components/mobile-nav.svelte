1a19b967e89041289f4e03b9a99a447bf5a914c0
André Miranda <aegis.andre@gmail.com>
2023-12-11 15:05:27 -0300

    feat: add mobile nav

---
+++ src/components/mobile-nav.svelte
@@ -0,0 +1,86 @@
+<script>
+  import DarkMode from "./dark-mode.svelte"
+
+  let isNavOpen = false
+
+  function closeMenu() {
+    isNavOpen = false
+  }
+</script>
+
+<nav class="relative flex items-center justify-end gap-4 sm:hidden">
+  <DarkMode />
+
+  <button
+    class="p-4"
+    aria-label={`${isNavOpen ? "open" : "close"} navigation menu`}
+    on:click={() => (isNavOpen = !isNavOpen)}
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class="h-8 w-8"
+      ><line x1="4" x2="20" y1="12" y2="12" /><line
+        x1="4"
+        x2="20"
+        y1="6"
+        y2="6"
+      /><line x1="4" x2="20" y1="18" y2="18" /></svg
+    >
+  </button>
+
+  {#if isNavOpen}
+    <ul
+      class="absolute left-0 top-0 flex h-screen w-full flex-col items-center justify-center
+       gap-4 bg-design-bg"
+      role="list"
+    >
+      <li>
+        <button
+          class="absolute right-0 top-0 p-4"
+          aria-label="close navigation menu"
+          on:click={closeMenu}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            class="h-8 w-8"><path d="M18 6 6 18" /><path d="m6 6 12 12" /></svg
+          >
+        </button>
+      </li>
+
+      <li><a on:click={closeMenu} href="#about">Sobre</a></li>
+
+      <li><a on:click={closeMenu} href="#featured">Destaques</a></li>
+
+      <li><a on:click={closeMenu} href="#projects">Projetos</a></li>
+
+      <li>
+        <a on:click={closeMenu} href="http://www.linkedin.com/in/andre-web-dev"
+          >Contato</a
+        >
+      </li>
+    </ul>
+  {/if}
+</nav>
+
+<style>
+  a {
+    display: inline-block;
+    padding: 1rem;
+  }
+</style>
