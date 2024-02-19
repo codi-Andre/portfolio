9e51d3bfa318d3cd2f7e57288848b07e16494c74
André Miranda <aegis.andre@gmail.com>
2024-02-19 12:26:55 -0300

    style: add container class strategy

---
+++ src/assets/icons/message-icon.tsx
@@ -0,0 +1,22 @@
+interface MessageIconProps {
+  class?: string
+}
+
+export function MessageIcon(props: MessageIconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+      class={props.class}
+    >
+      <path d="M7.9 20A9 9 0 1 0 4 16.1L2 22Z" />
+    </svg>
+  )
+}
