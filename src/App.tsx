0f9ba6eaf2c41ecbb7273927a7e5541a3ca9c424
André Miranda <aegis.andre@gmail.com>
2024-02-19 15:20:21 -0300

    feat: add hero section

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import styles from "./app.module.css"
 import { Header } from "./components/header/header"
+import { HeroSection } from "./components/hero/hero-section"
 
 function App() {
   return (
     <>
       <Header />
+      <main class="container">
+        <HeroSection />
+      </main>
     </>
   )
 }
---
+++ src/components/hero/hero-section.tsx
@@ -0,0 +1,27 @@
+import styles from "./hero-section.module.css"
+
+export function HeroSection() {
+  return (
+    <section class={styles.section}>
+      <img
+        class={styles["profile-pic"]}
+        src="/profile.webp"
+        alt="profile"
+        height={240}
+        width={240}
+      />
+
+      <div class={styles.wrapper}>
+        <p class={`${styles.subtitle}`}>Olá meu nome é André,</p>
+
+        <h1 class={`${styles.title} text-4xl`}>Desenvolvedor Front-end,</h1>
+
+        <p class={styles.description}>
+          eu construo websites e aplicativos web com lindas e amigáveis
+          interfaces que podem ser acessados por diferentes dispositivos e
+          plataformas.
+        </p>
+      </div>
+    </section>
+  )
+}
